170c02d18d93587b1f9d57c49c744c4d888e80cd
Manajot17 <mananjotsingh17@gmail.com>
2024-04-20 20:12:48 +0530

    2nd part commited

---
+++ client/src/components/SideBar/Conversations.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Conversation from './Conversation'
+
+function Conversations() {
+    return (
+        <div className='py-2 flex flex-col overflow-auto'>
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+        </div>
+    )
+}
+
+export default Conversations
+
+// STARTER CODE
+// export default Conversations
+
+
+// import React from 'react'
+// import Conversation from './Conversation'
+
+// function Conversations() {
+//     return (
+//         <div className='py-2 flex flex-col overflow-auto'>
+//             <Conversation />
+//             <Conversation />
+//             <Conversation />
+//             <Conversation />
+//             <Conversation />
+//             <Conversation />
+//         </div>
+//     )
+// }
+
+// export default Conversations
---
+++ client/src/components/SideBar/SearchInput.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { FaSearch } from "react-icons/fa";
+
+function SearchInput() {
+    return (
+        <form className='flex items-center gap-2'>
+            <input type="text" placeholder='Search' className='input input-bordered rounded-full' />
+            <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
+                <FaSearch />
+            </button>
+        </form>
+    )
+}
+
+export default SearchInput
+
+// STARTER CODE
+// export default SearchInput
+
+
+// import React from 'react'
+// import { FaSearch } from "react-icons/fa";
+
+// function SearchInput() {
+//     return (
+//         <form className='flex items-center gap-2'>
+//             <input type="text" placeholder='Search' className='input input-bordered rounded-full' />
+//             <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
+//                 <FaSearch />
+//             </button>
+//         </form>
+//     )
+// }
+
+// export default SearchInput
